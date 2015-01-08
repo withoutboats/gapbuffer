680f94a1c372562e4b49c4f0f82fafee4aea7766
David Lee Aronson <lee@libertad.ucsd.edu>
2015-01-08 03:37:27 -0800

    Removed unnecessary feature directives.

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,6 @@
 //  You should have received a copy of the GNU Lesser General Public License along with this
 //  program.  If not, see <http://www.gnu.org/licenses/>.
 #![feature(slicing_syntax)]
-#![feature(associated_types)]
-#![feature(default_type_params)]
 
 extern crate core;
 extern crate alloc;
